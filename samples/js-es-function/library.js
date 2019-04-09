dcd00873d9c0a18f6fd2eec2f01ab3414bd318e3
James Roper <james@jazzy.id.au>
2019-04-09 13:11:48 +0000

    More work

--- samples/js-es-function/library.js
+++ samples/js-es-function/library.js
@@ -1,74 +1,96 @@
-let PROTO_PATH = "protocol.proto";
+let protocolPath = "protocol.proto";
 let grpc = require("grpc");
 let protoLoader = require("@grpc/proto-loader");
-// Suggested options for similarity to existing grpc.load behavior
-let packageDefinition = protoLoader.loadSync(
-  PROTO_PATH,
-  {keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  });
-let descriptor = grpc.loadPackageDefinition(packageDefinition);
+let protobuf = require("protobufjs");
+
+function startEntity(options, initial, behavior) {
+  let packageDefinition = protoLoader.loadSync(
+    protocolPath,
+    {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true
+    });
+  let descriptor = grpc.loadPackageDefinition(packageDefinition);
+  let protocol = protobuf.loadSync(protocolPath);
 
 
-let Server = new grpc.Server();
 
-let InMessageCase = descriptor.EntityStreamIn.MessageCase;
+  let Server = new grpc.Server();
 
-function stream(call) {
+  let InMessageCase = descriptor.EntityStreamIn.MessageCase;
 
-  // The current entity state
-  var state;
+  function stream(call) {
 
-  // The current sequence number
-  var sequence;
+    // The current entity state
+    var state;
 
-  // The snapshot handler
-  var snapshotHandler;
+    // The current sequence number
+    var sequence;
 
-  // The event handler
-  var eventHandler;
+    // The snapshot handler
+    var snapshotHandler;
 
-  // The command handler
-  var commandHandler;
+    // The event handler
+    var eventHandler;
 
-  let context = {
-    getState: function() {
-      return state;
-    },
-    getSequence: function() {
-      return sequence;
-    },
-    setEventHander: function(handler) {
-      eventHandler = handler;
-    },
-    setCommandHandler: function(handler) {
-      commandHandler = handler;
-    }
-  };
+    // The command handler
+    var commandHandler;
 
-  call.on("data", function(entityStreamIn) {
+    let context = {
+      getState: function () {
+        return state;
+      },
+      getSequence: function () {
+        return sequence;
+      },
+      setEventHander: function (handler) {
+        eventHandler = handler;
+      },
+      setCommandHandler: function (handler) {
+        commandHandler = handler;
+      }
+    };
 
-    switch (entityStreamIn.getMessageCase()) {
-      case InMessageCase.INIT:
-        let init = entityStreamIn.getInit();
-        if (init.getSnapshot) {
+    call.on("data", function (entityStreamIn) {
 
-        }
-        break;
-      case InMessageCase.EVENT:
-        let event = entityStreamIn.getEvent();
-        break;
-      case InMessageCase.COMMAND:
-        let command = entityStreamIn.getCommand();
-        break;
-    }
+      switch (entityStreamIn.getMessageCase()) {
+        case InMessageCase.INIT:
+          let init = entityStreamIn.getInit();
+          if (init.getSnapshot) {
 
-  });
+          }
+          break;
+        case InMessageCase.EVENT:
+          let event = entityStreamIn.getEvent();
+          break;
+        case InMessageCase.COMMAND:
+          let command = entityStreamIn.getCommand();
+          break;
+      }
 
-  call.on("end", function() {
-    call.end();
-  });
+    });
+
+    call.on("end", function () {
+      call.end();
+    });
+  }
+
+
 }
+
+module.exports = class Entity {
+  setInitial(callback) {
+    this.initial = callback;
+  }
+
+  setBehavior(callback) {
+    this.behavior = behavior;
+  }
+
+  start(options) {
+    startEntity(options, this.initial, this.behavior)
+  }
+};
