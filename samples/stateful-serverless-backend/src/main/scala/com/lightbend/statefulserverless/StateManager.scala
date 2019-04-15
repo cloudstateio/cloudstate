3235550027994fce84021cb73e20041cb98cf336
Viktor Klang <viktor.klang@gmail.com>
2019-04-15 12:22:44 +0200

    Adding stubs and TODOs/FIXMEs to StateManager

--- samples/stateful-serverless-backend/src/main/scala/com/lightbend/statefulserverless/StateManager.scala
+++ samples/stateful-serverless-backend/src/main/scala/com/lightbend/statefulserverless/StateManager.scala
@@ -1,11 +1,45 @@
 package com.lightbend.statefulserverless
 
+import scala.concurrent.duration._
+import akka.actor._
 import akka.persistence.PersistentActor
+import akka.cluster.sharding.ShardRegion
 import com.lightbend.statefulserverless.grpc.{ Command, EntityClient }
+
+object StateManager {
+  final case object Stop
+}
 
 //FIXME IMPLEMENT
 final class StateManager(client: EntityClient) extends PersistentActor {
-  override final def receiveCommand: PartialFunction[Any, Unit] = ???
-  override final def receiveRecover: PartialFunction[Any, Unit] = ???
-  override final def persistenceId: String = "StateManager-" + self.path.name // FIXME derive name from actual proxied service?
+  // FIXME derive name from actual proxied service?
+  override final def persistenceId: String = "StateManager-" + self.path.name
+  // FIXME devise passivation strategy
+  import ShardRegion.Passivate
+  import StateManager.Stop
+
+  // TODO make configurable
+  context.setReceiveTimeout(15.minutes)
+
+  override final def receiveCommand: PartialFunction[Any, Unit] = {
+    case c: Command =>
+      /* TODOs
+        * Forward command to user logic
+          - val response = client.handle(???)
+        * Receive reply
+          - persist any resulting events
+          - persist any resulting snapshot
+          - handle any resulting failure
+        * Respond
+          - sender() ! response.payload
+      */
+    case ReceiveTimeout =>
+      context.parent ! Passivate(stopMessage = Stop)
+    case Stop           =>
+      context.stop(self)
+  }
+
+  override final def receiveRecover: PartialFunction[Any, Unit] = {
+    case _ => ??? // FIXME define and use events?
+  }
 }
