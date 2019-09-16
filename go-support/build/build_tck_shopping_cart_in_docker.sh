5b9df7c20b1da194d209f7a44b2bc29bdc601cd0
Viktor Klang <viktor.klang@gmail.com>
2019-09-16 14:35:44 +0200

    Hooking the Go compilation step into the sbt build and adding a dependency for the TCK

--- go-support/build/build_tck_shopping_cart_in_docker.sh
+++ go-support/build/build_tck_shopping_cart_in_docker.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-echo "building go TCK shoppingcart for OS: ${GOOS} on ${GOARCH}"
-
 SUFFIX=""
 if [ "$GOOS" == "windows" ]; then
   SUFFIX=".exe"
 fi
 
-cd shoppingcart/cmd/shoppingcart/ && CGO_ENABLED=0 go build -v -o "tck_shoppingcart${SUFFIX}"
+cd shoppingcart/cmd/shoppingcart/ && CGO_ENABLED=0 go build -v -o "tck_shoppingcart${SUFFIX}" && echo "go-support compiled successfully: ${GOOS} on ${GOARCH}"
