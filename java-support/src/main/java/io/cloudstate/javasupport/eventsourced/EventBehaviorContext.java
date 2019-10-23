04ea7e6d653b7b89d0f577a85b8dae79533287cc
Viktor Klang (√) <viktor.klang@gmail.com>
2019-10-23 12:45:11 +0000

    Removing the java-support Behavior API since it can be implemented us… (#132)
    Removing the java-support Behavior API since it can be implemented using branching

--- java-support/src/main/java/io/cloudstate/javasupport/eventsourced/EventBehaviorContext.java
+++
@@ -1,7 +0,0 @@
-package io.cloudstate.javasupport.eventsourced;
-
-/**
- * Event handler context that allows changing behavior. This can be passed to all {@link
- * EventHandler} annotated methods.
- */
-public interface EventBehaviorContext extends EventContext, BehaviorContext {}
--- java-support/src/main/java/io/cloudstate/javasupport/eventsourced/SnapshotBehaviorContext.java
+++
@@ -1,8 +0,0 @@
-package io.cloudstate.javasupport.eventsourced;
-
-/**
- * Snapshot context that allows changing behavior.
- *
- * <p>This may be passed to any {@link SnapshotHandler} annotated methods.
- */
-public interface SnapshotBehaviorContext extends SnapshotContext, BehaviorContext {}
