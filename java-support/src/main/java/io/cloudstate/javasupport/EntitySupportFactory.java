97d0e4667edd54390082704c82bfb3f4fd6af6e3
Adriano Santos <sleipnir@bsd.com.br>
2020-04-01 00:35:38 -0300

    Proposal to support to DI environments. Add java interfaces

--- java-support/src/main/java/io/cloudstate/javasupport/EntitySupportFactory.java
+++ java-support/src/main/java/io/cloudstate/javasupport/EntitySupportFactory.java
@@ -1,4 +1,18 @@
 package io.cloudstate.javasupport;
 
+/** Auxiliar interface for support DI environments */
 public interface EntitySupportFactory {
+  /**
+   * The reference to the Entity.
+   *
+   * @return The reference to the Entity.
+   */
+  Object create(Context context, String enittyId);
+
+  /**
+   * Return Class of Etity.
+   *
+   * @return The Class of Etity.
+   */
+  Class<?> typeClass();
 }
