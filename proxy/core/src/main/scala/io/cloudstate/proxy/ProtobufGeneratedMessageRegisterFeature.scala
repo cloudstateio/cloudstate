8887ee9a3b71916b71cf940e0e57fa712684fa84
Viktor Klang <viktor.klang@gmail.com>
2020-04-21 19:13:42 +0200

    Re-stating the AutomaticFeatures as for-comprehensions for clarity

--- proxy/core/src/main/scala/io/cloudstate/proxy/ProtobufGeneratedMessageRegisterFeature.scala
+++ proxy/core/src/main/scala/io/cloudstate/proxy/ProtobufGeneratedMessageRegisterFeature.scala
@@ -8,7 +8,7 @@ import scala.collection.JavaConverters._
 
 @AutomaticFeature
 final class ProtobufGeneratedMessageRegisterFeature extends Feature {
-  val messageClasses = Vector(
+  final val messageClasses = Vector(
     classOf[akka.protobuf.GeneratedMessage],
     classOf[akka.protobuf.GeneratedMessage.Builder[_]],
     classOf[akka.protobuf.ProtocolMessageEnum],
@@ -18,14 +18,13 @@ final class ProtobufGeneratedMessageRegisterFeature extends Feature {
     classOf[scalapb.GeneratedMessage]
   )
   override final def duringAnalysis(access: Feature.DuringAnalysisAccess) =
-    messageClasses.foreach { cls =>
-      if (access.isReachable(cls)) {
-        access.reachableSubtypes(cls).iterator.asScala.filter(_ != null) foreach {
-          case null => // Java…
-          case subtype =>
-            RuntimeReflection.register(subtype)
-            RuntimeReflection.register(subtype.getMethods: _*)
-        }
-      } else println("Could not register [" + cls.getName + "] because it was not found.")
+    for {
+      cls <- messageClasses.iterator
+      if access.isReachable(cls)
+      subtype <- access.reachableSubtypes(cls).iterator.asScala
+      if subtype != null
+    } {
+      RuntimeReflection.register(subtype)
+      RuntimeReflection.register(subtype.getMethods: _*)
     }
 }
