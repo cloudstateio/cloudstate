093cdbca75ac4215fd08ebb7ab208128ab31ccc3
Viktor Klang <viktor.klang@gmail.com>
2020-04-21 19:13:42 +0200

    Re-stating the AutomaticFeatures as for-comprehensions for clarity

--- proxy/core/src/main/scala/io/cloudstate/proxy/AkkaActorRegisterFeature.scala
+++ proxy/core/src/main/scala/io/cloudstate/proxy/AkkaActorRegisterFeature.scala
@@ -8,25 +8,23 @@ import scala.collection.JavaConverters._
 
 @AutomaticFeature
 final class AkkaActorRegisterFeature extends Feature {
-  val akkaActorClass = classOf[akka.actor.Actor]
-  override def duringAnalysis(access: Feature.DuringAnalysisAccess) =
+  final val akkaActorClass = classOf[akka.actor.Actor]
+  override final def duringAnalysis(access: Feature.DuringAnalysisAccess) =
     if (access.isReachable(akkaActorClass)) {
-      access.reachableSubtypes(akkaActorClass).iterator.asScala.filter(_ != null).foreach { subtype =>
-        if (subtype.getInterfaces.exists(_ == akkaActorClass)) {
-          for {
-            context <- getDeclaredField(subtype, "context")
-            self <- getDeclaredField(subtype, "self")
-          } {
-            RuntimeReflection.register(subtype)
-            RuntimeReflection.register( /* finalIsWritable = */ true, context, self)
-            //RuntimeReflection.register(subtype.getDeclaredFields: _*)
-            RuntimeReflection.register(subtype.getDeclaredConstructors: _*)
-          }
-        }
+      for {
+        subtype <- access.reachableSubtypes(akkaActorClass).iterator.asScala
+        if subtype != null && subtype.getInterfaces.exists(_ == akkaActorClass)
+        context <- getDeclaredField(subtype, "context")
+        self <- getDeclaredField(subtype, "self")
+      } {
+        RuntimeReflection.register(subtype)
+        RuntimeReflection.register( /* finalIsWritable = */ true, context, self)
+        //RuntimeReflection.register(subtype.getDeclaredFields: _*)
+        RuntimeReflection.register(subtype.getDeclaredConstructors: _*)
       }
     }
 
-  private def getDeclaredField(cls: Class[_], name: String) =
+  private[this] final def getDeclaredField(cls: Class[_], name: String) =
     try Option(cls.getDeclaredField(name))
     catch { case _: NoSuchFieldException => None }
 }
--- proxy/core/src/main/scala/io/cloudstate/proxy/AkkaSerializerRegisterFeature.scala
+++ proxy/core/src/main/scala/io/cloudstate/proxy/AkkaSerializerRegisterFeature.scala
@@ -8,18 +8,19 @@ import scala.collection.JavaConverters._
 
 @AutomaticFeature
 final class AkkaSerializerRegisterFeature extends Feature {
-  val akkaSerializerClass = classOf[akka.serialization.Serializer]
-  override def duringAnalysis(access: Feature.DuringAnalysisAccess) =
+  final val akkaSerializerClass = classOf[akka.serialization.Serializer]
+  override final def duringAnalysis(access: Feature.DuringAnalysisAccess) =
     if (access.isReachable(akkaSerializerClass)) {
-      access.reachableSubtypes(akkaSerializerClass).iterator.asScala.filter(_ != null).foreach { subtype =>
-        try {
-          val ctor = subtype.getDeclaredConstructor(classOf[akka.actor.ExtendedActorSystem])
-          RuntimeReflection.register(subtype)
-          RuntimeReflection.register(ctor)
-        } catch {
-          case nsme: NoSuchMethodException =>
-          // Ignore?
-        }
+      for {
+        subtype <- access.reachableSubtypes(akkaSerializerClass).iterator.asScala
+        if subtype != null
+        ctor <- getDeclaredConstructor(subtype)
+      } {
+        RuntimeReflection.register(subtype)
+        RuntimeReflection.register(ctor)
       }
     }
+  private[this] final def getDeclaredConstructor(cls: Class[_]) =
+    try Option(cls.getDeclaredConstructor(classOf[akka.actor.ExtendedActorSystem]))
+    catch { case _: NoSuchMethodException => None }
 }
