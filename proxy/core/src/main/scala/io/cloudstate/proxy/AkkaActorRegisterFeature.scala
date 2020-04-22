007e39b2c1fc1b467c7659a5880daf0ca2822ef4
Viktor Klang <viktor.klang@gmail.com>
2020-04-22 18:07:13 +0200

    Avoiding duplicate registration of actor classes

--- proxy/core/src/main/scala/io/cloudstate/proxy/AkkaActorRegisterFeature.scala
+++ proxy/core/src/main/scala/io/cloudstate/proxy/AkkaActorRegisterFeature.scala
@@ -8,16 +8,26 @@ import scala.collection.JavaConverters._
 
 @AutomaticFeature
 final class AkkaActorRegisterFeature extends Feature {
+  private[this] final var cache: Set[String] =
+    Set(
+      // FIXME the following are configured in akka-graal-config, remove this when migrating from it
+      "akka.io.TcpConnection",
+      "akka.io.TcpListener",
+      "akka.stream.impl.fusing.ActorGraphInterpreter"
+    )
+
   override final def duringAnalysis(access: Feature.DuringAnalysisAccess): Unit = {
     val akkaActorClass =
       access.findClassByName(classOf[akka.actor.Actor].getName) // We do this to get compile-time safety of the classes, and allow graalvm to resolve their names
     if (akkaActorClass != null && access.isReachable(akkaActorClass)) {
       for {
         subtype <- access.reachableSubtypes(akkaActorClass).iterator.asScala
-        if subtype != null
+        if subtype != null && !subtype.isInterface && !cache(subtype.getName)
+        _ = println("Automatically registering actor class for reflection purposes: " + subtype.getName)
         _ = RuntimeReflection.register(subtype)
         _ = RuntimeReflection.register(subtype.getDeclaredConstructors: _*)
-        //_ = RuntimeReflection.register(subtype.getDeclaredFields: _*)
+        _ = RuntimeReflection.register(subtype.getDeclaredFields: _*)
+        _ = cache += subtype.getName
         if subtype.getInterfaces.exists(_ == akkaActorClass)
         context <- getDeclaredField(subtype, "context")
         self <- getDeclaredField(subtype, "self")
