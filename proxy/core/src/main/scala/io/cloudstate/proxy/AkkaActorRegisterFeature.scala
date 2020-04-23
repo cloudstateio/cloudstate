54450de2a1b994566a0e8a2132bc7c38dbd54b05
Viktor Klang <viktor.klang@gmail.com>
2020-04-23 14:22:57 +0200

    Switching to a CHM-backed cached for the AutomaticFeatures to avoid race-conditions

--- proxy/core/src/main/scala/io/cloudstate/proxy/AkkaActorRegisterFeature.scala
+++ proxy/core/src/main/scala/io/cloudstate/proxy/AkkaActorRegisterFeature.scala
@@ -5,16 +5,18 @@ import org.graalvm.nativeimage.hosted.Feature.QueryReachabilityAccess
 import org.graalvm.nativeimage.hosted.{Feature, RuntimeReflection}
 
 import scala.collection.JavaConverters._
+import java.util.concurrent.ConcurrentHashMap
 
 @AutomaticFeature
 final class AkkaActorRegisterFeature extends Feature {
-  private[this] final var cache: Set[String] =
-    Set(
-      // FIXME the following are configured in akka-graal-config, remove this when migrating from it
-      "akka.io.TcpConnection",
-      "akka.io.TcpListener",
-      "akka.stream.impl.fusing.ActorGraphInterpreter"
-    )
+  private[this] final val cache = {
+    val c = ConcurrentHashMap.newKeySet[String]
+    // FIXME the following are configured in akka-graal-config, remove this when migrating from it
+    c.add("akka.io.TcpConnection")
+    c.add("akka.io.TcpListener")
+    c.add("akka.stream.impl.fusing.ActorGraphInterpreter")
+    c
+  }
 
   override final def duringAnalysis(access: Feature.DuringAnalysisAccess): Unit = {
     val akkaActorClass =
@@ -22,12 +24,11 @@ final class AkkaActorRegisterFeature extends Feature {
     if (akkaActorClass != null && access.isReachable(akkaActorClass)) {
       for {
         subtype <- access.reachableSubtypes(akkaActorClass).iterator.asScala
-        if subtype != null && !subtype.isInterface && !cache(subtype.getName)
+        if subtype != null && !subtype.isInterface && cache.add(subtype.getName)
         _ = println("Automatically registering actor class for reflection purposes: " + subtype.getName)
         _ = RuntimeReflection.register(subtype)
         _ = RuntimeReflection.register(subtype.getDeclaredConstructors: _*)
         _ = RuntimeReflection.register(subtype.getDeclaredFields: _*)
-        _ = cache += subtype.getName
         if subtype.getInterfaces.exists(_ == akkaActorClass)
         context <- getDeclaredField(subtype, "context")
         self <- getDeclaredField(subtype, "self")
