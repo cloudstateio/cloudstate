ec949b37e7faf5cbc82ae4909ce07c04b5b61ede
Viktor Klang (√) <viktor.klang@gmail.com>
2020-04-23 14:12:51 +0000

    Switching to a CHM-backed cached for the AutomaticFeatures to avoid race-conditions (#261)

--- proxy/core/src/main/scala/io/cloudstate/proxy/ProtobufGeneratedMessageRegisterFeature.scala
+++ proxy/core/src/main/scala/io/cloudstate/proxy/ProtobufGeneratedMessageRegisterFeature.scala
@@ -5,10 +5,11 @@ import org.graalvm.nativeimage.hosted.Feature.QueryReachabilityAccess
 import org.graalvm.nativeimage.hosted.{Feature, RuntimeReflection}
 
 import scala.collection.JavaConverters._
+import java.util.concurrent.ConcurrentHashMap
 
 @AutomaticFeature
 final class ProtobufGeneratedMessageRegisterFeature extends Feature {
-  private[this] final var cache: Set[String] = Set.empty
+  private[this] final val cache = ConcurrentHashMap.newKeySet[String]
   final val messageClasses = Vector(
     classOf[akka.protobuf.GeneratedMessage],
     classOf[akka.protobuf.GeneratedMessage.Builder[_]],
@@ -22,11 +23,10 @@ final class ProtobufGeneratedMessageRegisterFeature extends Feature {
     for {
       className <- messageClasses.iterator
       cls = access.findClassByName(className)
-      if cls != null && access.isReachable(cls) && !cache(cls.getName)
+      if cls != null && access.isReachable(cls) && cache.add(cls.getName)
       _ = println("Automatically registering protobuf message class for reflection purposes: " + cls.getName)
     } {
       RuntimeReflection.register(cls)
       RuntimeReflection.register(cls.getMethods: _*)
-      cache += cls.getName
     }
 }
