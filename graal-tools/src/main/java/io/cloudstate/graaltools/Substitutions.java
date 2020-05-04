e9b8bc18b96167e66d427e0afe408c980736e3a4
Srikanth Koneru <srikanth.koneru@lightbend.com>
2020-05-04 08:29:24 -0700

    remove io.grpc.netty.shaded.io.netty.util.internal.* substitutions

--- graal-tools/src/main/java/io/cloudstate/graaltools/Substitutions.java
+++ graal-tools/src/main/java/io/cloudstate/graaltools/Substitutions.java
@@ -41,43 +41,43 @@ final class Target_io_netty_util_internal_PlatformDependent0 {
       name = "address")
   private static long ADDRESS_FIELD_OFFSET;
 }
-
-@TargetClass(
-    className =
-        "io.grpc.netty.shaded.io.netty.util.internal.shaded.org.jctools.util.UnsafeRefArrayAccess")
-final
-class Target_io_grpc_netty_shaded_io_netty_util_internal_shaded_org_jctools_util_UnsafeRefArrayAccess {
-  @Alias
-  @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayIndexShift, declClass = Object[].class)
-  public static int REF_ELEMENT_SHIFT;
-}
-
-@TargetClass(className = "io.grpc.netty.shaded.io.netty.util.internal.CleanerJava6")
-final class Target_io_grpc_netty_shaded_io_netty_util_internal_CleanerJava6 {
-  @Alias
-  @RecomputeFieldValue(
-      kind = RecomputeFieldValue.Kind.FieldOffset,
-      declClassName = "java.nio.DirectByteBuffer",
-      name = "cleaner")
-  private static long CLEANER_FIELD_OFFSET;
-}
-
-@TargetClass(className = "io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent")
-final class Target_io_grpc_netty_shaded_io_netty_util_internal_PlatformDependent {
-  @Alias
-  @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayBaseOffset, declClass = byte[].class)
-  private static long BYTE_ARRAY_BASE_OFFSET;
-}
-
-@TargetClass(className = "io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent0")
-final class Target_io_grpc_netty_shaded_io_netty_util_internal_PlatformDependent0 {
-  @Alias
-  @RecomputeFieldValue(
-      kind = RecomputeFieldValue.Kind.FieldOffset,
-      declClassName = "java.nio.Buffer",
-      name = "address")
-  private static long ADDRESS_FIELD_OFFSET;
-}
+//
+//@TargetClass(
+//    className =
+//        "io.grpc.netty.shaded.io.netty.util.internal.shaded.org.jctools.util.UnsafeRefArrayAccess")
+//final
+//class Target_io_grpc_netty_shaded_io_netty_util_internal_shaded_org_jctools_util_UnsafeRefArrayAccess {
+//  @Alias
+//  @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayIndexShift, declClass = Object[].class)
+//  public static int REF_ELEMENT_SHIFT;
+//}
+//
+//@TargetClass(className = "io.grpc.netty.shaded.io.netty.util.internal.CleanerJava6")
+//final class Target_io_grpc_netty_shaded_io_netty_util_internal_CleanerJava6 {
+//  @Alias
+//  @RecomputeFieldValue(
+//      kind = RecomputeFieldValue.Kind.FieldOffset,
+//      declClassName = "java.nio.DirectByteBuffer",
+//      name = "cleaner")
+//  private static long CLEANER_FIELD_OFFSET;
+//}
+//
+//@TargetClass(className = "io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent")
+//final class Target_io_grpc_netty_shaded_io_netty_util_internal_PlatformDependent {
+//  @Alias
+//  @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayBaseOffset, declClass = byte[].class)
+//  private static long BYTE_ARRAY_BASE_OFFSET;
+//}
+//
+//@TargetClass(className = "io.grpc.netty.shaded.io.netty.util.internal.PlatformDependent0")
+//final class Target_io_grpc_netty_shaded_io_netty_util_internal_PlatformDependent0 {
+//  @Alias
+//  @RecomputeFieldValue(
+//      kind = RecomputeFieldValue.Kind.FieldOffset,
+//      declClassName = "java.nio.Buffer",
+//      name = "address")
+//  private static long ADDRESS_FIELD_OFFSET;
+//}
 
 @TargetClass(className = "org.agrona.concurrent.AbstractConcurrentArrayQueue")
 final class Target_org_agrona_concurrent_AbstractConcurrentArrayQueue {
