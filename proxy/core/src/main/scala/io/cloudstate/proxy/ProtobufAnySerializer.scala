4275270e7138dbca98bcf0f64def8a3d947e540d
Peter Vlugter <pvlugter@gmail.com>
2020-06-26 16:27:48 +1200

    Auto-create headers

--- proxy/core/src/main/scala/io/cloudstate/proxy/ProtobufAnySerializer.scala
+++ proxy/core/src/main/scala/io/cloudstate/proxy/ProtobufAnySerializer.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.cloudstate.proxy
 
 import akka.serialization.{BaseSerializer, SerializerWithStringManifest}
