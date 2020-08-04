3bc27d2863a79cea877386459a1e578c485f87c9
Marcel Lanz <marcellanz@n-1.ch>
2020-08-04 02:26:03 +0200

    [devcontainer] use docker devcontainer image.

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 
 CMD=${1:-image}
-DOCKER_BUILDKIT=1 docker build . -t cloudstateio/cloudstate-devcontainer:latest -f Dockerfile
+DOCKER_BUILDKIT=1 docker build . -t cloudstateio/cloudstate-devcontainer:latest
 if [ "$CMD" == "push" ]; then
-  docker push cloudstateios/cloudstate-devcontainer:latest
+  docker push cloudstateio/cloudstate-devcontainer:latest
 fi
