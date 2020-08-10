789a33a5306c355800025e88ed2d47484a3c5872
Marcel Lanz <marcellanz@n-1.ch>
2020-08-10 10:39:43 +0200

    [fix] buildkit usage removed.

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 
 CMD=${1:-image}
-DOCKER_BUILDKIT=1 docker build . -t cloudstateio/cloudstate-devcontainer:latest
+docker build . -t cloudstateio/cloudstate-devcontainer:latest
 if [ "$CMD" == "push" ]; then
   docker push cloudstateio/cloudstate-devcontainer:latest
 fi
